993f34fe241dec7d6d17e104ef63d577e5d8340c
erstinn <erstin.adean@gmail.com>
2023-03-10 16:10:45 +0800

    massive file structure revamp; setup confs for backend

---
+++ server/index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url"
+
+//  ===CONFIGS===
+// only when you use `type modules`
+const __filename =  fileURLToPath(import.meta.url); //to be able to use dir names
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit:"30mb", extended: true }));
+app.use(cors());
+// todo after the course, change the config below to store it somewhere e.g. in the cloud (mongoatlas)
+app.use("/assets", express.static(path.join(__dirname, 'public/assets'))); //sets dir where we keep our assets; e.g. images we will store
+
+// === FILE STORAGE ===
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, 'public/assets')
+    },
+    filename: function (req,file, cb){
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+
+// === MONGOOSE SETUP ===
+const PORT = process.env.PORT || 5172;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`))
---
+++ server/notes.md
@@ -0,0 +1,13 @@
+**Purpose of installed npm packages**
+- **body parser** to process request body 
+- **bcrypt** pw encryption
+- **cors** for cors
+- **dotenv** environment variables
+- **gridfs-stream** file upload //will probably not use for now
+- **multer** 
+- **multer-gridfs-storage** to upload your files and store them
+- **elmet** request safety
+- **morgan** logging
+- **jsonwebtoken** auth
+- **mongoose**
+- **express**
