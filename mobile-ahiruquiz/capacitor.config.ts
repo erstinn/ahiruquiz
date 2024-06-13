b5198b4e00d9f1cf15008f9e830703ffc731bfed
erstinn <erstin.adean@gmail.com>
2024-06-13 23:12:36 +0800

    ionic mobile lol

---
+++ mobile-ahiruquiz/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobile-ahiruquiz',
+  webDir: 'www' // where your web assets (HTML, CSS, JavaScript, etc.) are located
+};
+
+export default config;
