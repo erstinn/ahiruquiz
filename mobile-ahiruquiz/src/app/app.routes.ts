ef5e37e909932b5efee3479fa99ec9aaeb1e9d98
erstinn <erstin.adean@gmail.com>
2024-06-17 19:42:50 +0800

    fixed oopsie used ionic commands instead of ng

--- mobile-ahiruquiz/src/app/app.routes.ts
+++ mobile-ahiruquiz/src/app/app.routes.ts
@@ -10,4 +10,20 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'quiz',
+    loadComponent: () => import('./pages/quiz/quiz.page').then( m => m.QuizPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'ranking',
+    loadComponent: () => import('./pages/ranking/ranking.page').then( m => m.RankingPage)
+  },
 ];
